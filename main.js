5161d32d587b9a0423e12b5e62b1c878ef7a0dba
leesuyeon116 <suyeon873088@naver.com>
2024-07-10 17:17:54 +0900

    뉴스 본문 200자 초과 시 자동 생략 기능 추가

--- main.js
+++ main.js
@@ -20,7 +20,13 @@ const render = () => {
             </div>
             <div class="col-lg-8">
                 <h2>${news.title}</h2>
-                <p>${news.description}</p>
+                <p>${
+                    news.description == null || news.description == 
+                    "" ? "내용없음" 
+                    : news.description.length > 200 
+                    ? news.description.substring(0, 200) + "..." 
+                    : news.description
+                }</p>
                 <div>
                     ${news.source.name} * ${news.publishedAt}
                 </div>
