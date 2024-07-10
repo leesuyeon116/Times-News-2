f7cadcaf7bd921dc11ea9b2bfb317c4095d94814
leesuyeon116 <suyeon873088@naver.com>
2024-07-10 15:04:24 +0900

    url 수정 및 css start

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let news = [];
 
 const getLatestNews = async () => {
     // API 요청 URL 
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`); 
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`); 
     const response = await fetch(url); // API 요청 보내기
     const data = await response.json();
     news = data.articles;
