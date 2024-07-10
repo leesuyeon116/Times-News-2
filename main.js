52791cfad188f22fd705987d63f97fb08551fa29
leesuyeon116 <suyeon873088@naver.com>
2024-07-10 16:35:34 +0900

    뉴스 렌더링 구현

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 // 뉴스 API 키
 const API_KEY = 'c4bc2b6bdf3c4b1ebb90a08ee97275b4';
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
     // API 요청 URL 
     const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`); 
     const response = await fetch(url); // API 요청 보내기
     const data = await response.json();
-    news = data.articles;
-    console.log("ddd", data.articles);
+    newsList = data.articles;
+    render();
+    console.log("ddd", newsList);
 };
+
+const render = () => {
+    const newsHTML = newsList.map(
+        news => ` <div class="row news">
+            <div class="col-lg-4">
+                <img class="news-img-size" src=${news.urlToImage} />
+            </div>
+            <div class="col-lg-8">
+                <h2>${news.title}</h2>
+                <p>${news.description}</p>
+                <div>
+                    ${news.source.name} * ${news.publishedAt}
+                </div>
+            </div>
+        </div>`
+    ).join(""); 
+    console.log("html", newsHTML);
+    document.getElementById("news-board").innerHTML = newsHTML;
+};
+
 
 getLatestNews();
