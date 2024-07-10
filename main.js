c10264f11b3a334c2cead3b144d201cc1710e5c2
leesuyeon116 <suyeon873088@naver.com>
2024-07-10 23:42:18 +0900

    Code-Refactoring

--- main.js
+++ main.js
@@ -3,39 +3,35 @@ const API_KEY = 'c4bc2b6bdf3c4b1ebb90a08ee97275b4';
 let newsList = [];
 const menus = document.querySelectorAll(".menus button");
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
-
-const getLatestNews = async () => {
-    // API 요청 URL 
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`); 
+let url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?`);
+    
+const getNews = async() => {
     const response = await fetch(url); // API 요청 보내기
     const data = await response.json();
     newsList = data.articles;
     render();
-    console.log("ddd", newsList);
+};
+
+const getLatestNews = async () => {
+    // API 요청 URL 
+    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`); 
+    getNews();
 };
 
 // 카테고리별 검색
 const getNewsByCategory = async (event) => {
     const category = event.target.textContent.toLowerCase();
     console.log("category", category);
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category}`);
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("ddd", data);
-    newsList = data.articles;
-    render();
+    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category}`);
+    getNews();
 }
 
 // 키워드별 검색
 const getNewsByKeyword = async () => {
     const keyword = document.getElementById("search-input").value;
     console.log("keyword", keyword);
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=${keyword}`);
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("keyword data", data);
-    newsList = data.articles;
-    render();
+    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=${keyword}`);
+    getNews();
 }
 
 const render = () => {
